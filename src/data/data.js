f2e1efeb12040b7bce057e0b9521ab4241416a29
MsL-Mishal <106803397+MsL-Mishal@users.noreply.github.com>
2026-01-01 18:20:51 +0530

    feat: Add axios to additional skills and update skill levels of Redux Toolkit and TanStack Query

--- src/data/data.js
+++ src/data/data.js
@@ -125,8 +125,8 @@ export const skills = {
         { name: "HTML/CSS", level: 90 },
         { name: "Tailwind CSS", level: 85 },
         { name: "Shadcn", level: 80 },
-        { name: "Redux Toolkit", level: 75 },
-        { name: "TanStack Query", level: 70 },
+        { name: "Redux Toolkit", level: 85 },
+        { name: "TanStack Query", level: 80 },
     ],
     backend: [
         { name: "Node.js", level: 90 },
@@ -172,5 +172,6 @@ export const additionalSkills = [
     "Framer Motion",
     "Recoil",
     "Context API",
+    "Axios",
     "NextAuth.js",
 ];
